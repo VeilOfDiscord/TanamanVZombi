8395e77dd29b6d5c436b654261ca4eaa0122bcbc
Nathaell Avril Leman <84324372+VeilOfDiscord@users.noreply.github.com>
2024-11-10 18:43:32 -0500

    gun grabbable and fires bullet

---
+++ Assets/Assets/Scripts/fireBullet.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+public class fireBullet : MonoBehaviour
+{
+    [SerializeField] public GameObject bullet;
+    [SerializeField] public Transform spawnPoint;
+    [SerializeField] public float fireSpeed = 20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable grabbable = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>();
+        grabbable.activated.AddListener(FireBullet);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FireBullet(ActivateEventArgs arg)
+    {
+        GameObject spawnedBullet = Instantiate(bullet);
+        spawnedBullet.transform.position = spawnPoint.position;
+        spawnedBullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * fireSpeed;
+        Destroy(spawnedBullet, 5);
+    }
+}
